67a268000e9e4d3f19e0aed314353b488a66a8fe
Jose Nuñez <josefns24@gmail.com>
2024-08-28 08:39:37 +0200

    Realizamos un CRUD

---
+++ src/main/java/com/fran/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.fran.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fran.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+    
+}
